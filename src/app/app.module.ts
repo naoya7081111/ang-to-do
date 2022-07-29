951938dc8665455bd96cb8034697f40992024961
藤川尚也 <naoya7081111@gmail.com>
2022-07-29 22:35:25 +0900

    ang-3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './pages/header/header.component';
 import { SidebarComponent } from './pages/sidebar/sidebar.component';
+import { TaskCardComponent } from './pages/task-card/task-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SidebarComponent
+    SidebarComponent,
+    TaskCardComponent
   ],
   imports: [
     BrowserModule,
